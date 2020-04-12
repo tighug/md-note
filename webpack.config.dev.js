974e1e9b0a386e5c14cf2ad28e23c6acf81881ce
tighug <kapuriko123@gmail.com>
2020-04-13 01:08:58 +0900

    chore: Add webpack.config.dev.js file

---
+++ webpack.config.dev.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const { HotModuleReplacementPlugin } = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { spawn } = require("child_process");
+
+module.exports = {
+  target: "electron-renderer",
+  mode: "development",
+  entry: "./src/renderer/index.tsx",
+  output: {
+    path: path.join(__dirname, "build/renderer"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [{ test: /\.tsx$/, exclude: /node_modules/, use: "ts-loader" }],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [
+    new HotModuleReplacementPlugin({
+      multiStep: true,
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "src/renderer/index.html",
+    }),
+  ],
+  devServer: {
+    compress: true,
+    watchContentBase: true,
+    contentBase: path.join(__dirname, "build/renderer"),
+    headers: { "Access-Control-Allow-Origin": "*" },
+    historyApiFallback: {
+      verbose: true,
+      disableDotRule: false,
+    },
+    hot: true,
+    inline: true,
+    noInfo: true,
+    port: 3000,
+    publicPath: "http://localhost:3000/build/",
+    watchOptions: {
+      agreedTimeout: 300,
+      ignored: /node_modules/,
+      poll: 100,
+    },
+    before() {
+      console.log("Starting Main Process...");
+      spawn("npm", ["run", "start-main-dev"], {
+        shell: true,
+        env: process.env,
+        stdio: "inherit",
+      })
+        .on("close", (code) => process.exit(code))
+        .on("error", (spawnError) => console.error(spawnError));
+    },
+  },
+};
