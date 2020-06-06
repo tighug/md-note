5fa3bba133c2810e30f4afe4f1cd4b7bcae9b120
tighug <kapuriko123@gmail.com>
2020-06-06 13:35:57 +0900

    feat: Add App container

---
+++ src/renderer/containers/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+// import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import theme from "../theme/dark/default";
+// import store from "../store";
+// import EditorPage from "../components/pages/EditorPage";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      {/* <Provider store={store}>
+        <EditorPage />
+      </Provider> */}
+    </ThemeProvider>
+  );
+}
+
+export default hot(App);
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
+import App from "./containers/App";
+import "ress";
+import "typeface-roboto";
+import "./index.css";
 
 document.addEventListener("DOMContentLoaded", () =>
-  render(<p>Hello, World!</p>, document.getElementById("root"))
+  render(<App />, document.getElementById("root"))
 );
