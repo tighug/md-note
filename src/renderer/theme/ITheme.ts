cd90224baa4d01fb27b3659f6022f7253690724a
tighug <kapuriko123@gmail.com>
2020-06-06 13:45:09 +0900

    feat: Add dark theme

---
+++ src/renderer/theme/ITheme.ts
@@ -0,0 +1,17 @@
+export default interface ITheme {
+  color: {
+    primary: string;
+    secondary: string;
+    normal: string;
+    bg: {
+      primary: string;
+      secondary: string;
+      disabled: string;
+    };
+    text: {
+      primary: string;
+      secondary: string;
+      disabled: string;
+    };
+  };
+}
---
+++ src/renderer/theme/dark/default.ts
@@ -0,0 +1,21 @@
+import ITheme from "../ITheme";
+
+const theme: ITheme = {
+  color: {
+    primary: "mediumseagreen",
+    secondary: "orangered",
+    normal: "#272727",
+    bg: {
+      primary: "#424242",
+      secondary: "#303030",
+      disabled: "rgba(255, 255, 255, 0.12);",
+    },
+    text: {
+      primary: "rgba(255, 255, 255, 1)",
+      secondary: "rgba(255, 255, 255, 0.8)",
+      disabled: "rgba(255, 255, 255, 0.3)",
+    },
+  },
+};
+
+export default theme;
